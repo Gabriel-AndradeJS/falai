c3b7e6b6b8328519748d599f74bc7c6d4f969b8c
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-05-03 10:14:56 -0300

    feat: implement AuthTokenGuard for JWT token validation and user status check

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,7 +13,57 @@ export class AuthService {
         private prisma: PrismaService,
         private readonly hashingService: HashingServiceProtocol,
 
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+        private readonly jwtService: JwtService,
+
     ) {}
+
+
+    async signIn (signInDto: SignInDto) {
+        const { email, password } = signInDto;
+        const user = await this.prisma.user.findFirst({
+            where: { 
+                email,
+                active: true,
+             },
+        });
+
+        await this.prisma.user.update({
+            where: { email },
+            data: { status: 'online' },
+        });
+
+
+        if (!user) {
+            throw new HttpException('Usuario não encontrado', HttpStatus.NOT_FOUND);
+        }
+
+        const isPasswordValid = await this.hashingService.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            throw new HttpException('Senha/Usuario incorreta', HttpStatus.UNAUTHORIZED);
+        }
+
+        const token = this.jwtService.sign({ sub: user.id, email: user.email }, {
+            secret: this.jwtConfiguration.secret,
+                expiresIn: this.jwtConfiguration.jwtTtl,
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+        });
+
+        return { 
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                status: user.status,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+                token:  token,
+         };
+    }
+
+
 
     async signUp (signInDto: SignInDto) {
         const { email, password, name } = signInDto;
