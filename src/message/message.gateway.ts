b7d1b08ea114f494a4ba7341372074ce05116f62
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-05-06 16:30:34 -0300

    feat: implement MessageGateway for WebSocket message handling

---
+++ src/message/message.gateway.ts
@@ -0,0 +1,28 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessageService } from './message.service';
+import { Server } from 'http';
+import { OnModuleInit } from '@nestjs/common';
+
+@WebSocketGateway()
+export class MessageGateway implements OnModuleInit {
+  @WebSocketServer() 
+  server: Server;
+
+  constructor(private readonly messageService: MessageService) {}
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+    });
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() payload: {
+    senderId: number;
+    receiverId: number;
+    content: string;
+  }) {
+    // this.messageService.createMessage(payload.senderId, payload.receiverId, payload.content);
+    this.server.emit('message', payload.content);
+  }
+
+}
