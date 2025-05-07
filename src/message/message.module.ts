eceea00d42ae5d08375b3d5b3699f1edd843403d
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-05-06 21:05:00 -0300

    feat: add MessageController to MessageModule for message handling

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,9 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageGateway } from './message.gateway';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MessageController } from './message.controller';
 
 @Module({
   imports: [PrismaModule],
-  providers: [MessageService, MessageGateway]
+  providers: [MessageService, MessageGateway],
+  controllers: [MessageController],
 })
 export class MessageModule {}
