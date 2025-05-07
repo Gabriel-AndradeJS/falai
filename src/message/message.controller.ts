90cb10751d8e5d2e77ac3acd918d0167d45f2646
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2025-05-06 21:04:49 -0300

    feat: add MessageController for retrieving messages between users with authentication

---
+++ src/message/message.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
+import { MessageService } from './message.service';
+import { TokenPayloadParam } from 'src/auth/param/token-payload.param';
+import { PayloadTokenDto } from 'src/auth/dto/payload-token.dto';
+import { AuthTokenGuard } from 'src/auth/guard/auth-token.guard';
+
+@Controller('message')
+export class MessageController {
+    constructor(private readonly messageService: MessageService) {}
+
+    @UseGuards(AuthTokenGuard)
+    @Get()
+    getAllMessages(@TokenPayloadParam() tokenPayloadParam: PayloadTokenDto, @Query('receiver', ParseIntPipe) receiver: number) {
+        return this.messageService.getMessagesBetweenUsers(tokenPayloadParam, receiver);
+    }
+    
+}
